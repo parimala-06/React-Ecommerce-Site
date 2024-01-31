95a5c145d8db6cee85b1b5b1f942d8cf01ccf852
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-31 15:40:06 +0530

    Added elements in Product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,58 @@
 import React from "react";
+import './Css/Product.css';
+import img1 from '../Components/Assets/ProductFront1.jpg';
+import img2 from '../Components/Assets/ProductBack.png';
+import { IoStar } from "react-icons/io5";
+import { IoStarHalf } from "react-icons/io5";
+
 
 function Product(){
-    return(
-        <div>
 
+    function addDays() {
+        var result = new Date();
+        result.setDate(result.getDate() + 3);
+        return result.toDateString();
+      }
+    const date = addDays();
+    return(
+        <div className="productdisplay">
+            <div className="productdisplay-left">
+                <div className="productdisplay-img-list">
+                    <img src={img1} alt="front"/>
+                    <img src={img2} alt="back"/>
+                </div>
+                <div className="productdisplay-img">
+                    <img className="productdisplay-main-img" src={img1} alt="main"/>
+                </div>
+            </div>
+            <div className="productdisplay-right">
+                <h1>VitoBa Multi Cereal Health Mix</h1>
+                <div className="productdisplay-right-star">
+                    <IoStar />
+                    <IoStar />
+                    <IoStar />
+                    <IoStar />
+                    <IoStarHalf />
+                    <p>(200)</p>
+                </div>
+                <div className="productdisplay-right-prices">
+                    <div className="old-price">&#8377;500</div>
+                    <div className="new-price">&#8377;350</div>
+                </div>
+                <div className="productdisplay-right-description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </div>
+                <div className="productdisplay-right-quant">
+                        <h2>Select Quantity</h2>
+                        <div className="productdisplay-right-quants">
+                        <div>250g</div>
+                        <div>500g</div>
+                        <div>1kg</div>
+                    </div>
+                </div>
+                <button>ADD TO CART</button>
+                <p className="productdisplay-right-date">Get it by,<span> {date}</span></p>
+            </div>
         </div>
     );
 }
