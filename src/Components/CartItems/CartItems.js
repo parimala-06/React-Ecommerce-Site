fe8645eca65d7ca926c711a21d386d4dd6e7d545
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-01-31 17:36:19 +0530

    Added cart pages

---
+++ src/Components/CartItems/CartItems.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './CartItems.css';
+
+
+function CartItems(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+export default CartItems;
--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Glorify from "../Components/Glorify/Glorify";
+import Product from "./Product";
 
 function Shop(){
     return(
         <div>
             <Glorify/>
+            <Product/>
         </div>
     );
 }
