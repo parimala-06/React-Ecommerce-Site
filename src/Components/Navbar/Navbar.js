0a4d55e8890c7da2ac8f671ad9c6aa36d582fc14
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-02-04 14:40:55 +0530

    Minor Changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,19 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import './Navbar.css';
 import trials from '../Assets/trials2.png';
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext.js";
+import { IoIosMenu } from "react-icons/io";
 
 function Navbar() {
     const {getTotalCartItem} = useContext(ShopContext);
+    const buttonRef = useRef();
+
+    function dropDownToggle(e){
+        buttonRef.current.classList.toggle('nav-login-cart-visible');
+        e.target.classList.toggle('open');
+    }
     return(
         <div className="navbar">
             <div className="nav-logo">
@@ -14,7 +21,8 @@ function Navbar() {
                 <img src={trials} alt="Logo"/>
             </Link>
             </div>
-            <div className="nav-login-cart">
+            <IoIosMenu className='nav-dropdown' onClick={dropDownToggle} size={30}/>
+            <div ref={buttonRef} className="nav-login-cart">
                 <Link style={{textDecoration: 'none'}} to='/goodness'><button>Goodness</button></Link>
                 <Link style={{textDecoration: 'none'}} to='/display'><button>Products</button></Link>
                 <Link style={{textDecoration: 'none'}} to='/login'><button>Login</button></Link>
